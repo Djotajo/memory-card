a30bc636c57145642c917fcc4c2e93329a9a3536
Djotajo <20823564+Djotajo@users.noreply.github.com>
2024-07-03 19:38:40 +0200

    Implement deck rotation on card click

--- src/displayCard.jsx
+++ src/displayCard.jsx
@@ -23,13 +23,43 @@ function DisplayCard({
     let item = event.target.alt;
     shuffleDeck();
     updatePrevCards(prevCards, setPrevCards, item);
+
+    let allCards = document.querySelectorAll(".card");
+    let cardsArray = [...allCards]; // converts NodeList to Array
+
+    let allCardsContent = document.querySelectorAll(".cardImg, .cardName");
+    let allCardsContentArray = [...allCardsContent];
+
+    // cardsArray.forEach((item) => {
+    //   item.classList.remove("flip");
+    // });
+
+    cardsArray.forEach((item) => {
+      allCardsContentArray.forEach(
+        (card) => (card.style.visibility = "hidden")
+      );
+      // item.style.animation = "none";
+      // item.offsetHeight;
+      // item.style.animation = null;
+      item.classList.remove("flip");
+      void item.offsetWidth;
+      item.classList.add("flip");
+
+      item.addEventListener("animationend", function handleAnimationEnd() {
+        item.classList.remove("flip");
+        item.removeEventListener("animationend", handleAnimationEnd);
+        allCardsContentArray.forEach(
+          (card) => (card.style.visibility = "visible")
+        );
+      });
+    });
   }
 
   useEffect(() => {
     async function fetchPokeUrl() {
       const pokeData = await getData(pokeID);
       setPokeUrl(pokeData.sprites.front_default);
-      setPokeName(pokeData.name.toUpperCase());
+      setPokeName(pokeData.species.name.toUpperCase());
     }
 
     fetchPokeUrl();
